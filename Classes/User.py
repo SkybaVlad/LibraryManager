d0ae967cd303de836a1223f02e96608dc79febe5
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-02 20:42:12 +0300

    Create eq and hash methods in User Class

--- Classes/User.py
+++ Classes/User.py
@@ -2,3 +2,13 @@ class User:
     def __init__(self,name,user_id):
         self.name = name
         self.user_id = user_id
+
+    def __eq__(self,other):
+        if isinstance(other,User):
+            if self.user_id == other.user_id:
+                if self.name == other.name:
+                    return True
+        return False
+
+    def __hash__(self):
+        return hash(self.user_id)
