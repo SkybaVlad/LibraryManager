e7d04be65c48d1dbc2251b2448f68ef2e0a3c110
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-02 19:05:55 +0300

    Add extend Classes. to import and 3 unittests for init method and add_books method

---
+++ Testing/test_Manager.py
@@ -0,0 +1,18 @@
+import unittest
+from Classes.Book import Book
+from Classes.Manager import Manager
+
+class TestManager(unittest.TestCase):
+    def test_valid_data(self):
+        m = Manager()
+        self.assertEqual(m.books, [])
+        self.assertEqual(m.users, {})
+
+    def test_add_book(self):
+        m = Manager()
+        book_obj = Book("HarryPotter",1)
+        m.add_book(book_obj.name,book_obj.quantity)
+        added_book = m.books[0]
+        self.assertEqual(added_book.name,book_obj.name)
+        self.assertEqual(added_book.quantity,book_obj.quantity)
+
