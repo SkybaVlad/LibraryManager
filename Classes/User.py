22fabfe5bfded7ba42b3194aa7d356c6a43a1408
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-06 18:54:50 +0300

    Change a lot

--- Classes/User.py
+++ Classes/User.py
@@ -1,10 +1,12 @@
 class User:
-    def __init__(self,name,user_id):
+    def __init__(self, name, user_id, email, password):
         self.name = name
         self.user_id = user_id
+        self.email = email
+        self.password = password
 
-    def __eq__(self,other):
-        if isinstance(other,User):
+    def __eq__(self, other):
+        if isinstance(other, User):
             if self.user_id == other.user_id:
                 if self.name == other.name:
                     return True
