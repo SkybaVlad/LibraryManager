17c6110f9e64aeefd2722b8d14f67eda586ea9ed
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-06 20:11:48 +0300

    Add fields to testing

--- Testing/test_User.py
+++ Testing/test_User.py
@@ -1,8 +1,15 @@
 import unittest
 from Classes.User import User
 
+
 class TestUser(unittest.TestCase):
-    def init_user(self):
-        user_obj = User("John",3)
+    def test_init_user(self):
+        user_obj = User("John", 3, 'john@gmail', 'pass1')
         self.assertEqual(user_obj.name, "John")
-        self.assertEqual(user_obj.user_id,3)
+        self.assertEqual(user_obj.user_id, 3)
+        self.assertEqual(user_obj.email, 'john@gmail')
+        self.assertEqual(user_obj.password, 'pass1')
+
+
+if __name__ == '__main__':
+    unittest.main()
