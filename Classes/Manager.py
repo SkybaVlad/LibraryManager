b784f743567f3fba52801397bb1ae4788c62f88b
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-06 20:14:10 +0300

    Edit borrow method

--- Classes/Manager.py
+++ Classes/Manager.py
@@ -17,16 +17,10 @@ class Manager:
         search_book(book_obj.book_name)
 
     def borrow_book(self, user_obj, book_obj):
-        for user in self.users:
-            if user.user_id == user_id:
-                for book in self.books:
-                    if book.name == book_name:
-                        self.users[user] = book_name
-                        return book
-        return None
+        borrow_book(user_obj, book_obj)
 
     def print_all_users(self):
         get_all_users()
 
     def print_all_books(self):
-        pass
+        get_all_books()
